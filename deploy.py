e49a98bc11341db5654af8b48d834a2e56c6301d
Laisha Wadhwa <laishawadhwa98@gmail.com>
2023-09-28 21:06:33 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,31 +1,40 @@
 import json
 from web3 import Web3, HTTPProvider
-from web3.contract import ConciseContract
+# from web3.contract import ConciseContract
+import os
+from eth_account import Account
+from eth_account.signers.local import LocalAccount
+from web3 import EthereumTesterProvider
+from web3.middleware import construct_sign_and_send_raw_middleware
 
 # web3.py instance
-w3 = Web3(HTTPProvider("https://ropsten.infura.io/v3/<API key>"))
-print(w3.isConnected())
+infura_url = "https://polygon-mumbai.infura.io/v3/<YOUR API KEY>"
+w3 = Web3(Web3.HTTPProvider(infura_url))
 
-key="<Private Key here with 0x prefix>"
-acct = w3.eth.account.privateKeyToAccount(key)
+# w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+print(w3.is_connected())
+
+private_key="<Your Private Key>"
+acct: LocalAccount = Account.from_key(private_key)
 
 # compile your smart contract with truffle first
-truffleFile = json.load(open('./build/contracts/greeter.json'))
+truffleFile = json.load(open('./build/contracts/<contract name>.json'))
 abi = truffleFile['abi']
 bytecode = truffleFile['bytecode']
 contract= w3.eth.contract(bytecode=bytecode, abi=abi)
 
 #building transaction
-construct_txn = contract.constructor().buildTransaction({
+construct_txn = contract.constructor().build_transaction({
     'from': acct.address,
-    'nonce': w3.eth.getTransactionCount(acct.address),
+    'nonce': w3.eth.get_transaction_count(acct.address) + 3,
     'gas': 1728712,
-    'gasPrice': w3.toWei('21', 'gwei')})
+    'gasPrice': w3.to_wei('65', 'gwei')})
 
-signed = acct.signTransaction(construct_txn)
+signed = acct.sign_transaction(construct_txn)
 
-tx_hash=w3.eth.sendRawTransaction(signed.rawTransaction)
+tx_hash=w3.eth.send_raw_transaction(signed.rawTransaction)
 print(tx_hash.hex())
-tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-print("Contract Deployed At:", tx_receipt['contractAddress'])
+# tx_receipt = w3.eth.get_transaction_receipt(tx_hash)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Contract Deployed At:", tx_receipt['status'])
 
